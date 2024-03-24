978b877391f6255470f244dfa6768b56a3c312f8
Mike <camikhail@yandex.ru>
2024-03-24 16:44:16 +0300

    Добавил бег влево, частично оптимизировал перемещение, поменял фон

--- Project.py
+++ Project.py
@@ -1,48 +1,59 @@
 import pygame
 clock = pygame.time.Clock()
 pygame.init()
-screen = pygame.display.set_mode((1200,845)) # flags = pygame.NOFRAME - убрать рамку у окна
-pygame.display.set_caption('#') # задать название для окна
-background = pygame.image.load('background/background.png') # задаем задний фон
+screen = pygame.display.set_mode((768, 416)) # flags = pygame.NOFRAME - убрать рамку у окна
+pygame.display.set_caption('Game') # задать название для окна
+background = pygame.image.load('background/Map.png') # задаем задний фон
 
-hero_stay = [pygame.image.load('hero/hero_go_right/Idle__000.png'),
-pygame.image.load('hero/hero_go_right/Idle__000.png'),
-pygame.image.load('hero/hero_go_right/Idle__000.png'),
-pygame.image.load('hero/hero_go_right/Idle__001.png'),
-pygame.image.load('hero/hero_go_right/Idle__001.png'),
-pygame.image.load('hero/hero_go_right/Idle__001.png'),
-pygame.image.load('hero/hero_go_right/Idle__002.png'),
-pygame.image.load('hero/hero_go_right/Idle__002.png'),
-pygame.image.load('hero/hero_go_right/Idle__002.png'),
-pygame.image.load('hero/hero_go_right/Idle__003.png'),
-pygame.image.load('hero/hero_go_right/Idle__003.png'),
-pygame.image.load('hero/hero_go_right/Idle__003.png'),
-pygame.image.load('hero/hero_go_right/Idle__004.png'),
-pygame.image.load('hero/hero_go_right/Idle__004.png'),
-pygame.image.load('hero/hero_go_right/Idle__004.png'),
-pygame.image.load('hero/hero_go_right/Idle__005.png'),
-pygame.image.load('hero/hero_go_right/Idle__005.png'),
-pygame.image.load('hero/hero_go_right/Idle__005.png'),
-pygame.image.load('hero/hero_go_right/Idle__006.png'),
-pygame.image.load('hero/hero_go_right/Idle__006.png'),
-pygame.image.load('hero/hero_go_right/Idle__006.png'),
-pygame.image.load('hero/hero_go_right/Idle__007.png'),
-pygame.image.load('hero/hero_go_right/Idle__007.png'),
-pygame.image.load('hero/hero_go_right/Idle__007.png'),
-pygame.image.load('hero/hero_go_right/Idle__008.png'),
-pygame.image.load('hero/hero_go_right/Idle__008.png'),
-pygame.image.load('hero/hero_go_right/Idle__008.png'),
+hero_stay = [pygame.image.load('hero/Движение/На месте/Idle__000.png'),
+pygame.image.load('hero/Движение/На месте/Idle__000.png'),
+pygame.image.load('hero/Движение/На месте/Idle__000.png'),
+pygame.image.load('hero/Движение/На месте/Idle__001.png'),
+pygame.image.load('hero/Движение/На месте/Idle__001.png'),
+pygame.image.load('hero/Движение/На месте/Idle__001.png'),
+pygame.image.load('hero/Движение/На месте/Idle__002.png'),
+pygame.image.load('hero/Движение/На месте/Idle__002.png'),
+pygame.image.load('hero/Движение/На месте/Idle__002.png'),
+pygame.image.load('hero/Движение/На месте/Idle__003.png'),
+pygame.image.load('hero/Движение/На месте/Idle__003.png'),
+pygame.image.load('hero/Движение/На месте/Idle__003.png'),
+pygame.image.load('hero/Движение/На месте/Idle__004.png'),
+pygame.image.load('hero/Движение/На месте/Idle__004.png'),
+pygame.image.load('hero/Движение/На месте/Idle__004.png'),
+pygame.image.load('hero/Движение/На месте/Idle__005.png'),
+pygame.image.load('hero/Движение/На месте/Idle__005.png'),
+pygame.image.load('hero/Движение/На месте/Idle__005.png'),
+pygame.image.load('hero/Движение/На месте/Idle__006.png'),
+pygame.image.load('hero/Движение/На месте/Idle__006.png'),
+pygame.image.load('hero/Движение/На месте/Idle__006.png'),
+pygame.image.load('hero/Движение/На месте/Idle__007.png'),
+pygame.image.load('hero/Движение/На месте/Idle__007.png'),
+pygame.image.load('hero/Движение/На месте/Idle__007.png'),
+pygame.image.load('hero/Движение/На месте/Idle__008.png'),
+pygame.image.load('hero/Движение/На месте/Idle__008.png'),
+pygame.image.load('hero/Движение/На месте/Idle__008.png'),
 ]
-hero_run_r = [pygame.image.load('hero/hero_go_right/Run__000.png'), # все картинки для анимации героя(идет вправо)
-pygame.image.load('hero/hero_go_right/Run__001.png'),
-pygame.image.load('hero/hero_go_right/Run__002.png'),
-pygame.image.load('hero/hero_go_right/Run__003.png'),
-pygame.image.load('hero/hero_go_right/Run__004.png'),
-pygame.image.load('hero/hero_go_right/Run__005.png'),
-pygame.image.load('hero/hero_go_right/Run__006.png'),
-pygame.image.load('hero/hero_go_right/Run__007.png'),
-pygame.image.load('hero/hero_go_right/Run__008.png'),
-pygame.image.load('hero/hero_go_right/Run__009.png'),
+hero_run_right = [pygame.image.load('hero/Движение/Бег вправо/Run__000.png'), # все картинки для анимации героя(идет вправо)
+pygame.image.load('hero/Движение/Бег вправо/Run__001.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__002.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__003.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__004.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__005.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__006.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__007.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__008.png'),
+pygame.image.load('hero/Движение/Бег вправо/Run__009.png'),
+]
+hero_run_left = [pygame.image.load('hero/Движение/Бег влево/Run__000.png'), # все картинки для анимации героя(идет вправо)
+pygame.image.load('hero/Движение/Бег влево/Run__001.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__002.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__003.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__004.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__005.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__006.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__007.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__008.png'),
+pygame.image.load('hero/Движение/Бег влево/Run__009.png'),
 ]
 hero_jump = [pygame.image.load('hero/hero_go_right/Jump__000.png'),
 pygame.image.load('hero/hero_go_right/Jump__001.png'),
@@ -114,36 +125,30 @@ is_jump = False
 jump = 10
 jump_count = 0
 hero_speed = 5
-hero_pos_x = 70
-hero_pos_y = 700
+hero_pos_x = 20
+hero_pos_y = 282
 stay_count = 0
 run_r_count = 0 # счетчик картинок
 bg_x = 0 # начальное положение заднего фона по х
 running = True # основной цикл игры
 while running:
     clock.tick(30) # частота обновления (если задать значение больше все действия будут быстрее?)
-    screen.blit(background,(bg_x,0))
-    screen.blit(background, (bg_x+1200, 0)) # здесь мы циклически ставим один задний фон за другим
+    screen.blit(background,(bg_x,0)) #(этот код нам нужен, если у нас будет одна большая карта, а не зацикленная на одном месте картинка?)
+    # screen.blit(background, (bg_x+768, 0)) # здесь мы циклически ставим один задний фон за другим
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        bg_x +=2
+        screen.blit(hero_run_left[run_r_count % 10], (hero_pos_x, hero_pos_y))
+        run_r_count += 1
         hero_pos_x -= hero_speed
+        bg_x +=2
     elif keys[pygame.K_d]:
-        screen.blit(hero_run_r[run_r_count], (hero_pos_x, hero_pos_y))
-        if run_r_count == len(
-                hero_run_r) - 1:  # если картинки в списке walk_right закончились , обнулить счетчик walk_right_count , начать заново
-            run_r_count = 0
-        else:
-            run_r_count += 1
-        hero_pos_x+=hero_speed
+        screen.blit(hero_run_right[run_r_count%10], (hero_pos_x, hero_pos_y))
+        run_r_count += 1
+        hero_pos_x +=hero_speed
         bg_x-=2
     else:
-        screen.blit(hero_stay[stay_count], (hero_pos_x,hero_pos_y)) # перебираем картинки анимации
-        if stay_count == len(
-                hero_stay) - 1:  # если картинки в списке walk_right закончились , обнулить счетчик walk_right_count , начать заново
-            stay_count = 0
-        else:
-            stay_count += 1
+        screen.blit(hero_stay[stay_count%27], (hero_pos_x,hero_pos_y)) # перебираем картинки анимации
+        stay_count += 1
     if not is_jump:
         if keys[pygame.K_w]:
             is_jump = True
@@ -162,7 +167,7 @@ while running:
         bg_x = 0
 
     pygame.display.update()
-    for event in pygame.event.get(): # перебрать все возможные ивент в pygame , если quit - закрыть окно, или проще если жмем красный крест на окне - закрыть окно
+    for event in pygame.event.get(): # перебрать все возможные ивент в pygame, если quit - закрыть окно, или проще если жмем красный крест на окне - закрыть окно
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
