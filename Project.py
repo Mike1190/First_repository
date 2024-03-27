3d09325303751ee77109522589468833617cea16
артем <artem.2211@yandex.ru>
2024-03-27 19:25:26 +0300

    27.03 рабочая версия

--- Project.py
+++ Project.py
@@ -5,7 +5,7 @@ W, H = 1200, 840
 screen = pygame.display.set_mode((W, H))  # flags = pygame.NOFRAME - убрать рамку у окна
 clock = pygame.time.Clock()
 fps = 60
-hero = Object(30,700,3,'hero/hero_go_right/Run__000.png')
+hero = Object(30,700,3,'hero/Movement/Staing/Idle__000.png')
 # pygame.display.set_caption('#') # задать название для окна
 background = pygame.image.load('background/Background.png')
 bg_x = 0
@@ -20,37 +20,34 @@ while True:
             hero.jump = False   #0
             hero.slide = False
             hero.stay = True    #1
-
+            hero.walk = False  # 0
+            hero.left = False  # 0
+            hero.right = False
     key = pygame.key.get_pressed()
     if key[pygame.K_d]:
         hero.rect.x +=1
         hero.walk = True    #1
         hero.right = True   #1
-        hero.left = False   #0
         bg_x -= 2
     if key[pygame.K_a]:
         hero.rect.x-=1
         hero.walk = True    #1
         hero.left = True    #1
-        hero.right = False  #0
+
         bg_x += 2
     if key[pygame.K_w]:
-        hero.walk = False   #0
-        hero.left = False   #0
-        hero.right = False  #0
+
         hero.jump = True    #1
         if hero.on_ground:
             hero.dy-=20
             hero.on_ground = False  #0
     if key[pygame.K_s]:
         hero.slide = True
-        hero.rect.x += 7
-        hero.dy = 5
-
+        hero.rect.x += 5
 
     screen.blit(background,(bg_x,0))
     screen.blit(background, (bg_x + 1200, 0))
-    hero.update(H-hero.rect.h)
+    hero.update(H-90)
     screen.blit(hero.image,hero.rect)
       # передвигать каждую итерацию главного цикла задний фон на 2 пикселя?
     if bg_x == -1200:
